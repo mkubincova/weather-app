9c3d85e51c576ce8f74a5bd49f5eba4830fc0802
mkubincova <mkubincova@proton.me>
2023-06-16 09:51:35 +0200

    feat: navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      "weather-primary": "#00668A",
-      "weather-secondary": "#004E71",
+      colors:{
+        "weather-primary": "#00668A",
+        "weather-secondary": "#004E71",
+      }
     },
     fontFamily: {
       Roboto: ["Roboto, sans-serif"],
