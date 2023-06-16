819513d6d1fb10a4b66369cd8d9cddd3816b40e5
mkubincova <mkubincova@proton.me>
2023-06-16 12:19:33 +0200

    feat: city search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,47 @@
-<script setup>
-</script>
-
 <template>
-  <main>
-   
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input type="text" placeholder="Search for a city or state" v-model="searchQuery" @input="getSearchResults"
+        class="w-full py-2 px-2 bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+      <ul class="abosolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults">
+        <p v-if="searchError">Oops, something went wrong, please try again.</p>
+        <p v-if="!searchError && mapboxSearchResults.length === 0">Nothing in here, try a different query.
+        </p>
+        <template v-else>
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+            {{ searchResult.place_name }}</li>
+        </template>
+      </ul>
+    </div>
   </main>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const mapboxAPIkey = "pk.eyJ1IjoibWt1YmluY292YSIsImEiOiJjbGl5Y2R1MmcwNmx0M2xzOWoxOG9jeHJ6In0.P_Jm3IFNSEI7emDHqmt7Cg";
+const searchQuery = ref('');
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+const searchError = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIkey}&types=place`);
+        mapboxSearchResults.value = result.data.features;
+      } catch {
+        console.log(searchError.value);
+        searchError.value = true;
+      }
+
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300);
+};
+</script>
