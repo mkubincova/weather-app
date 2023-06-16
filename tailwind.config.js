39761489921f62909b34f076808abe64471784d1
mkubincova <mkubincova@proton.me>
2023-06-16 09:29:56 +0200

    feat: initial configuration

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      "weather-primary": "#00668A",
+      "weather-secondary": "#004E71",
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    }
+  },
+  plugins: [],
+}
+
